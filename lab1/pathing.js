fc105ab7f7a093f0c91397c71c47f6cd6d2a4a98
Elvin Holst <elvho551@student.liu.se>
2024-09-04 21:44:06 +0200

    Skapade routes för POST och PATCH

--- lab1/pathing.js
+++ lab1/pathing.js
@@ -3,22 +3,52 @@ import * as db from "./database.js";
 
 const app = express();
 
+/*
+    TODO:
+        - Returkoder:
+            - 200 för POST och PATCH om OK
+            - 400 för POST och PATCH om felaktiga parametrar
+            - 405 om fel metod
+            - 404 om sidan ej finns
+            - 500 för alla andra fel
+        - Lägg in meddelande
+        - Sätt meddelande som läst/oläst
+        - Validera meddelanden i backend
+        - Krascha inte om felaktig JSON
+*/
+
+/*
+    curl-kommandon:
+    $ curl -H 'Content-Type: application/json' -d '{ "message": "hallå" }' http://localhost:3000/messages -X POST
+    $ curl -H 'Content-Type: application/json' -d '{ "read": "false" }' http://localhost:3000/messages/0 -X PATCH
+*/
+
+// Behövs för att parsa JSON-requests 
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log("hej");
     next();
+});
+
+app.get('/messages', async (req, res) => {
+    let msgs = await db.get_all_messages();
+    res.send(msgs);
 });
 
 app.get('/messages/:id', async (req, res) => {
     let id = parseInt(req.params.id);
     let msg = await db.read_message(id);
-    console.log(msg)
     res.send(msg)
 });
 
-app.get('/messages', async (req, res) => {
-    let msgs = await db.get_all_messages();
-    //msgs.forEach((e) => (e));
-    res.send(msgs);
+app.post('/messages', (req, res) => {
+    console.log(req.body.message)
+    res.status(200).send();
+});
+
+app.patch('/messages/:id', async (req, res) => {
+    console.log(req.body.read);
+    res.status(200).send();
 });
 
 const port = 3000;
