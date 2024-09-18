8a7997e2bc76c9500e70ae159b23f5a00d14a3ca
Elvin Holst <elvho551@student.liu.se>
2024-09-18 10:27:46 +0200

    Cookies

--- lab2/script.js
+++ lab2/script.js
@@ -6,19 +6,137 @@
 //     alert("aaaa");
 // })
 
+/*
+        <li class="post">
+            <div class="author">
+                Author
+            </div>
+            <div class="middle">
+                <div class="message">
+                    Message
+                </div>
+
+                <input type="checkbox" class="read">
+            </div>
+            <div class="time">
+                Time
+            </div>
+        </li>  
+*/
+
+
+function get_messages()
+{
+    let cookie = `; ${document.cookie}`.match("messages=([^;]+)");
+
+    if (cookie === null) {
+        return [];
+    }
+
+    let result = JSON.parse(cookie[1]);
+
+    if (Array.isArray(result) === false) {
+        result = [result];
+    }
+
+    return result;
+}
+
+function render_messages() 
+{
+    let list = document.getElementById("message_list");
+    list.innerHTML = "";
+
+    let message_list = get_messages();
+
+    message_list.forEach(element => {
+        add_message(element);
+    });
+}
+
+window.onload = () => {
+    render_messages();
+}
+
+function save_to_cookie(messages)
+{
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "messages=" + JSON.stringify(messages);
+    console.log(JSON.stringify(messages));
+
+    render_messages();
+}
+
+function add_message(msg)
+{
+    let item = document.createElement("li");
+    item.setAttribute("class", "post");
+
+    let author = document.createElement("div");
+    author.setAttribute("class", "author");
+    author.innerHTML = msg.author;
+    item.appendChild(author);
+
+    let middle = document.createElement("div");
+    middle.setAttribute("class", "middle");
+
+    let message = document.createElement("div");
+    message.setAttribute("class", "message");
+    message.innerHTML = msg.message;
+
+    middle.appendChild(message);
+
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("class", "read");
+
+    middle.appendChild(checkbox);
+    
+    item.appendChild(middle);
+
+    let time = document.createElement("div");
+    time.setAttribute("class", "time");
+    date = new Date(msg.time);
+    date_string = date.getFullYear() + "-" + (date.getMonth() + 1) +
+    "-" + date.getDate() + " " + date.getHours() + ":" +
+    date.getMinutes();
+    time.innerHTML = "Posted on " + date_string;
+    item.appendChild(time);
+
+    document.getElementById("message_list").appendChild(item);
+}
+
 document.getElementById('post').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
     let author = document.getElementById("Author").value;
     let msg = document.getElementById("Message").value;
-    let  = {
-        "author" : author,
-        "message" : msg
+
+    error_msg = ""
+    if (msg.length === 0) {
+        // Log the values to the console (or handle them as needed)
+        error_msg = "Message can not be empty.";
+    } else if (msg.length > 140) {
+        error_msg = "Message can not be longer than 140 characters.";
+    } else {
+        let request = {
+            "author": (author.length === 0) ? "Author" : author,
+            "message": msg,
+            "time": Date.now(),
+            "read": "false"
+        }
+
+        let messages = get_messages();
+        messages.unshift(request);
+        console.log(messages);
+        save_to_cookie(messages);
+
+        return;
     }
 
-    // Log the values to the console (or handle them as needed)
-    alert("hello");
-    console.log(o);
+    let error = document.getElementById("error_msg");
+    error.textContent = "Error";
+
 });
 
 // function formPost() {
